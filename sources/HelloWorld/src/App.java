f1677faf6a53a6505d3b61efee6953538be90dac
Mint Rosetta <150432237+mintrosetta@users.noreply.github.com>
2024-08-21 23:30:54 +0700

    Rules of Java program and input

--- sources/HelloWorld/src/App.java
+++ sources/HelloWorld/src/App.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    // args = argumentse
+    public static void main(String[] args) {
+        // Create selt introduction program
+        /*
+         * Multiple line commments
+         */
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please provide any input: ");
+        String text = scanner.nextLine();
+
+        scanner.close();
+
+        System.out.println("Hello, My name is Chanokchon Wongjampa");
+        System.out.println("I'm living in Bangkok, Thailand");
+        System.out.println("User input: " + text);
     }
 }
