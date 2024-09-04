945d9ec3fd346defc1a4fef83ac10ce3f1f055f9
Mint Rosetta <150432237+mintrosetta@users.noreply.github.com>
2024-09-04 23:54:48 +0700

    Create Operator.java

---
+++ sources/HelloWorld/src/Operator.java
@@ -0,0 +1,33 @@
+public class Operator {
+    public static void main(String[] args) {
+
+    }
+
+    public static  void arithmetic() {
+
+    }
+
+    public static  void relational() {
+
+    }
+
+    public static  void logical() {
+
+    }
+
+    public static  void bitwise() {
+
+    }
+
+    public static  void assignmentOperator() {
+
+    }
+
+    public static  void unaryOperator() {
+
+    }
+
+    public static  void ternary() {
+
+    }
+}
