5f90a11dd87a52657b6fc8aa76eacd16c7d8eaf5
Mint Rosetta <150432237+mintrosetta@users.noreply.github.com>
2024-09-14 15:48:24 +0700

    Update PrimeNumberInTheArray.java

--- sources/HelloWorld/src/Assignment2/PrimeNumberInTheArray.java
+++ sources/HelloWorld/src/Assignment2/PrimeNumberInTheArray.java
@@ -1,5 +1,8 @@
 package Assignment2;
 
+// prime คือ เลขที่มีแค่เลข 1 กับตัวมันเองที่หารลงตัวเท่านั้น
 public class PrimeNumberInTheArray {
-    
+    public static void main(String[] args) {
+        
+    }
 }
